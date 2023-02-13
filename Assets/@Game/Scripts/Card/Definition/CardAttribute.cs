cd403768ddc1e17691100b2bf5862d1e9770beac
sanyoni <ksyline1997@gmail.com>
2023-02-14 01:31:12 +0900

    카드를 정의하는 클래스들을 추가합니다.

---
+++ Assets/@Game/Scripts/Card/Definition/CardAttribute.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardAttribute : ScriptableObject
+{
+    [SerializeField] private string m_Name;
+    [SerializeField] private Texture2D m_Thumbnail;
+    [SerializeField] private int m_Cost;
+    [SerializeField] private ECardType m_Type;
+    [SerializeField] private CardPrerequisite m_Prerequisite;
+    [SerializeField] private List<CardOperationBase> m_OperationSequence;
+
+    public string GetName() => m_Name;
+    public Texture2D GetThumbnail() => m_Thumbnail;
+    public int GetCost() => m_Cost;
+    public ECardType GetType() => m_Type;
+    public CardPrerequisite GetPrerequisite() => m_Prerequisite;
+    public List<CardOperationBase> GetOperationSequence() => m_OperationSequence;
+}
--- Assets/@Game/Scripts/Enums.cs
+++ Assets/@Game/Scripts/Enums.cs
@@ -13,3 +13,19 @@ public enum EOrderedDummyType
     JunkDummy_Mine,
     JunkDummy_SpecificOpponent,
 }
+
+public enum ECardType
+{
+    Princess = 0x01,
+    Action = 0x02,
+    Attack = 0x04,
+    Defense = 0x08,
+    Territory = 0x10,
+    Succession = 0x20,
+    Calamity = 0x40,
+    Subtype_Military = 0x80,
+    Subtype_Magic = 0x100,
+    Subtype_Maid = 0x200,
+    Main = Princess | Action | Attack | Defense | Territory | Succession | Calamity,
+    Subtype = Subtype_Military | Subtype_Magic | Subtype_Maid
+}
