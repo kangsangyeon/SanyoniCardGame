5e22d3dac203465e56a5b0933840fd37e92053ac
sanyoni <ksyline1997@gmail.com>
2023-02-14 01:46:12 +0900

    GameManager 클래스를 작성합니다.

---
+++ Assets/@Game/Scripts/CardField.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections.Generic;
+
+public class CardField
+{
+    private List<Card> m_CardList = new List<Card>();
+}
--- Assets/@Game/Scripts/GameManager.cs
+++ Assets/@Game/Scripts/GameManager.cs
@@ -1,5 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    struct MarketPair
+    {
+        public CardAttribute cardAttribute;
+        public int count;
+    }
+
+    private Dictionary<int, CardDummy> m_PlayerDeckDict;
+    private Dictionary<int, CardDummy> m_PlayerJunkDummyDict;
+    private Dictionary<int, CardField> m_PlayerFieldDict;
+    private CardDummy m_SupplyDummy;
+    private MarketPair[] m_MarketList = new MarketPair[8];
 }
