8bc391e1db787acb55d92c5d006e09a02a1bf148
sanyoni <ksyline1997@gmail.com>
2023-03-06 02:06:09 +0900

    파발마 카드 효과를 추가합니다.

--- Assets/@Game/Scripts/CardEffect/Definition/Operation_Impl/Cost2/CardOperation_2_Horse.cs
+++ Assets/@Game/Scripts/CardEffect/Definition/Operation_Impl/Cost2/CardOperation_2_Horse.cs
@@ -6,6 +6,12 @@ public class CardOperation_2_Horse : CardOperationBase
 {
     public override IEnumerator Perform(CardGameObject _owner)
     {
+        // 내 더미에서 한 장을 가져옵니다.
+        var _card = GameManager.Instance.GetPlayerDummy(0).Draw(1);
+        
+        // 내 손패에 넣습니다.
+        GameManager.Instance.GetPlayerHandDummy(0).AddCardList(_card);
+
         yield return null;
     }
 }
--- Assets/@Game/Scripts/GameManager.cs
+++ Assets/@Game/Scripts/GameManager.cs
@@ -15,12 +15,14 @@ public class GameManager : MonoBehaviour
     [SerializeField] private UI_CardDummy m_UI_CardDummy;
 
     [SerializeField] private CardDummy[] m_PlayerHandDummyList = new CardDummy[2];
+    [SerializeField] private CardDummy[] m_PlayerDummyList = new CardDummy[2];
     [SerializeField] private CardDummy m_SupplyDummy;
     [SerializeField] private CardDummy m_OutlandDummy;
     private MarketPair[] m_MarketList = new MarketPair[8];
 
     public UI_CardDummy GetUICardDummy() => m_UI_CardDummy;
     public CardDummy GetPlayerHandDummy(int _index) => m_PlayerHandDummyList[_index];
+    public CardDummy GetPlayerDummy(int _index) => m_PlayerDummyList[_index];
     public CardDummy GetSupplyDummy() => m_SupplyDummy;
     public CardDummy GetOutlandDummy() => m_OutlandDummy;
 
