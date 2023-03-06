bb925bad56de122c8986e138681a8b00c18a21cb
sanyoni <ksyline1997@gmail.com>
2023-03-07 02:00:42 +0900

    docs: 로그에 카드명이 올바르게 담기도록 수정합니다.

--- Assets/@Game/Scripts/GameObject/CardDummy/Impl/CardFieldLink.cs
+++ Assets/@Game/Scripts/GameObject/CardDummy/Impl/CardFieldLink.cs
@@ -54,7 +54,7 @@ public class CardFieldLink : MonoBehaviour
         Card _card = _cardDrag.GetCardGO().GetCard();
         m_Card = _card;
 
-        Debug.Log($"CardFieldLink::OnDrop Called. \"{gameObject.name}\" from \"{_card.GetDummy().name}\" to \"{gameObject.name}\"");
+        Debug.Log($"CardFieldLink::OnDrop Called. \"{_cardDrag.GetCardGO().ToString()}\" from \"{_card.GetDummy().name}\" to \"{gameObject.name}\"");
 
         // 이 FieldLink에 카드를 올려둘 수 없도록 막습니다.
         m_DropZone.SetCanDrop(false);
